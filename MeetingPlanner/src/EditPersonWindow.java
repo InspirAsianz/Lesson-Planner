ae6ffd494ebb8378ed5f3d720c52b833a620b1d8
andrewma5 <44853183+andrewma5@users.noreply.github.com>
2021-08-15 20:48:04 -0700

    Update EditPersonWindow.java

--- MeetingPlanner/src/EditPersonWindow.java
+++ MeetingPlanner/src/EditPersonWindow.java
@@ -1,5 +1,27 @@
-public class EditPersonWindow {
-public static void main(String[] args) {
-	System.out.println("eeee");
-}
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.*;
+
+public class EditPersonWindow implements Runnable {
+
+	@Override
+	public void run() {
+		int w = 500;
+		int h = 400;
+
+		JFrame frame = new JFrame();
+		frame.setLayout(null);		
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		frame.setLocation(dim.width/2 - w/2, dim.height/2 - h/2);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Settings");
+		frame.pack();
+		frame.setVisible(true);
+		frame.setResizable(false);
+	}
+	
+	public void main(String[] args) {
+		SwingUtilities.invokeLater(new EditPersonWindow());
+	}
 }
